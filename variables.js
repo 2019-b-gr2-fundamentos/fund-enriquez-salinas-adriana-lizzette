9f3be8060c5ea34792b48ae0635250f1b4bba8bf
Adriana Enríquez <adriana.enriquez@epn.edu.ec>
2019-10-17 18:02:35 -0500

    Crear variables

---
+++ variables.js
@@ -0,0 +1,34 @@
+/*
+Nombre: Adriana Enriquez
+Fecha actualizacion: 17-10-2019
+Descripcion: Ejemplos de variables primitivas en JS
+*/
+
+//Comentario
+/* <- abro bloque comentario 
+cierro bloque comentario _. */
+
+// Primitivas (tipos de variables)
+/*
+int       -> Numero entero       Ej: 5 o -3
+char      -> Caracter            Ej: a o b o c
+float     -> Numeros Decimales   Ej:  12.12 o -14.2
+double    -> N. Decimales ++     Ej: 1212.........
+string    -> Arreglo de char     Ej: "abc" o "cd"
+null      -> VACIO               Ej: null
+boolean   -> Verdadero/falso     Ej: true o false
+*/
+
+//var edad = 18; // No se utiliza ya 
+let edad = 18; // Mutable -> Cambiar
+// const edad = 18; // Inmutable -> No Cambiar 
+const IVA = 12;
+//IVA = 14; // NO REASIGNAR VALORES 
+let sueldo = 0;
+// edad = 30 -> python
+// var edad = Integer = 30; -> kotlin Mutable
+// val edad = Integer = 30; -> kotlin Inmutabe
+
+// EN LO POSIBLE DECLARAR VARIABLES COMO INMUTABLE //
+//let IVA = 12;
+//const IVA = 12;
