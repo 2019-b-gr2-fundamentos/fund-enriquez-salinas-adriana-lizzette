dd18f45b00bb144220aab30a6a3e4dd6864f2ae0
Adriana Enriquez <adriana.enriquez@epn.edu.ec>
2019-10-21 15:55:35 -0500

    Crear variables tipadas

---
+++ Variables2.ts
@@ -0,0 +1,20 @@
+//Variables Primitivas
+const numero = 10 
+const decimal = 10.12
+const respuestaTipoNumero = typeof numero;
+console.log(respuestaTipoNumero)
+console.log(typeof decimal); 
+const letra = "a";
+const palabra = "palabra";
+console.log(typeof letra);
+console.log(typeof palabra);
+const hijos = null;
+console.log(typeof hijos);
+const casado = false;
+console.log(typeof casado)
+const fecha = new Date ();
+console.log(typeof fecha)
+// Gestor de paquetes
+// de nodejs -> comunicarse con el 
+// repositorio -> Descargar el "paquete"
+//npm -> yarn
