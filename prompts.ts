94eee3719c0930ce8385f20ff9937eb65e38cffe
Adriana Enriquez <adriana.enriquez@epn.edu.ec>
2019-10-30 17:48:14 -0500

    calculadora jsjs

--- prompts.ts
+++ prompts.ts
@@ -1,7 +1,11 @@
 const numeroUnoString = prompt("Ingresa numero 1"); //STRING
 const numeroDosString = prompt("Ingresa numero 2"); //STRING
+var signoString = prompt("¿Que operacion quieres")
 const numeroUno = Number(numeroUnoString);
 const numeroDos = Number(numeroDosString);
-console.log(numeroUno + numeroDos);
+if (signoString = "suma"){
+    console.log(numeroUno + numeroDos)    
+}
+
 
 // 3 y 4 hace que se operen los numeros  como numeros y no como strings
