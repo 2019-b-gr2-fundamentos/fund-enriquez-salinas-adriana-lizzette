204bff8c240de5bd9859b60a1d729d66b2b10959
Adriana Enríquez <adriana.enriquez@epn.edu.ec>
2019-11-06 14:14:21 -0500

    ejemplos if php

---
+++ ejemplos-else-if.php
@@ -0,0 +1,31 @@
+<?php
+//Si el valor de galletas es verdadero, lanza el valor "si hay galletas", si no, lanza "no hay galletas"
+$galletas = true{
+    echo 'Hay galletas';
+}else {
+    echo 'No hay galletas'
+}
+?>
+
+<?php
+//compara edades de personas para saber si es nene, adolescente o joven
+$edad = 20;
+if(0=>$edadedad>12){
+    echo 'Es un nene';
+}else if(12=>$edad>18){
+    echo 'Es un adolescente';
+}else if(18=>$edad){
+    echo 'Es un adulto'
+}
+?>
+
+<?php
+//salta si un valor es mayor que b o no 
+if($a>$b){
+    echo 'a es mayor que b';
+}else if ($a == $b){
+    echo 'a es igual a b';
+}else{
+    echo 'a es menor que b'
+}
+?>
