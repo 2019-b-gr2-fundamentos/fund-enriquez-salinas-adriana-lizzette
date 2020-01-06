5a27b7fada9ff5717ba22de5938acdb4e447ae20
Adriana Enriquez <adriana.enriquez@epn.edu.ec>
2020-01-06 16:04:15 -0500

    librerias

---
+++ Proyecto/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+//node modules
+var sumaLibreria = require("libreria-ae/lib/suma");
+// const sumaLibreria = require('libreria-ae/lib/suma');
+var respuesta = sumaLibreria(1, 2);
+console.log("La respuesta es:", respuesta);
