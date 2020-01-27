10cab1325f98a5a9d2c756326244d7a81cdace39
Adriana <adriana.enriquez@epn.edu.ec>
2020-01-27 13:08:29 -0500

    Para no tener que hacer tanto commit
    Pinche wea que no se guarda te odio

---
+++ Deber para el lunes/interfaces/interfacePersonajes.ts
@@ -0,0 +1,7 @@
+export interface matrizPersonajes {
+    Nombre:string,
+    Raza:string,
+    Edad:number, // indentificacion, las edades varian muchisimo 
+    Ciudad:string,
+    Oficio:string
+}
